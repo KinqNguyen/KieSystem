1fcf4d2d96c4ca6685c383a9463cf06edd640c16
Kiet.nguyen <kiet.nguyen@aegona.com>
2024-07-08 16:32:17 +0700

    Init push

---
+++ KieSystem/Controllers/CustomController.cs
@@ -0,0 +1,42 @@
+﻿using KieSystem.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace KieSystem.Controllers
+{
+
+    [Obsolete("This controller is deprecated and should not be used.")]
+    public class ObsoleteControllerBase : ControllerBase
+    {
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomController : ObsoleteControllerBase
+    {
+
+
+        [HttpGet(Name = "get")]
+        public IEnumerable<BlogClass> Get() { 
+        
+            IEnumerable<BlogClass> list = autogenerate(100);
+            return list;
+        }
+
+        private List<BlogClass> autogenerate(int number) {
+            List<BlogClass> list = new List<BlogClass>();
+            for (int i = 0; i < number; i++) {
+                list.Add(new BlogClass
+                {
+                    Id = i,
+                    Title = "Title " + i.ToString(),
+                    Body = "BOdy " + i.ToString(),
+                    UserId = 1,
+                });
+            }
+            return list;
+        }
+    }
+
+
+}
---
+++ KieSystem/DTOs/UserClass.cs
@@ -0,0 +1,10 @@
+﻿namespace KieSystem.DTOs
+{
+    public class BlogClass
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+}
+}
